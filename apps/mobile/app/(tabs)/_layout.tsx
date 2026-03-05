0be68a54355b2c479859f17a271fb20b5e5b6270
wisdomabioye <wisdomabioye@gmail.com>
2026-03-05 14:53:28 +0100

    feat(TabsLayout): add backBehavior prop to Tabs component

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -61,6 +61,7 @@ export default function TabsLayout() {
 
   return (
     <Tabs
+      backBehavior="history"
       screenOptions={() => ({
         headerShown: false,
         tabBarActiveTintColor: theme.colors.primary,
