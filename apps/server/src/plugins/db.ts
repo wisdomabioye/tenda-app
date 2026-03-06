ef966a2d37abbb41a333604035c9d567d438fba6
wisdomabioye <wisdomabioye@gmail.com>
2026-03-06 23:11:24 +0100

    feat: update database connection to resolve hostname dynamically

--- apps/server/src/plugins/db.ts
+++ apps/server/src/plugins/db.ts
@@ -1,6 +1,8 @@
 import fp from 'fastify-plugin'
 import postgres from 'postgres'
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import { lookup } from 'node:dns/promises'
+import { URL } from 'node:url'
 import {
   users,
   gigs,
@@ -31,7 +33,10 @@ declare module 'fastify' {
 
 export default fp(
   async (fastify) => {
-    const sql = postgres(getConfig().DATABASE_URL)
+    const dbUrl = new URL(getConfig().DATABASE_URL)
+    const { address } = await lookup(dbUrl.hostname, { family: 4 })
+    dbUrl.hostname = address
+    const sql = postgres(dbUrl.toString())
     const db = drizzle(sql, { schema })
 
     fastify.decorate('db', db)
